55878d4d04cf354e6d980453cb99a55c83a3ac0d
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-07-14 22:40:46 +0900

    2010 年と 2015 年のデータから「集計年」「都道府県」「15〜19歳の人口」を抜き出す

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({'input': rs,'output':{} });
 rl.on('line',(lineString) =>{
-    console.log(lineString) ;
+    const colums = lineString.split(',');
+    const year = parseInt(colums[0]);
+    const prefecture = colums[2];
+    const popu = parseInt(colums[7]);
+    if(year === 2010 || year ===　2015){
+        console.log(year);
+        console.log(prefecture);
+        console.log(popu);
+    }
 });
 rl.resume();
