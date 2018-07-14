571cfd608fc8865521d15dc71a3485b23a25c4a2
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-07-14 23:27:28 +0900

    変化率の計算

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ const fs = require("fs");
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({'input': rs,'output':{} });
+const map = new Map();
 rl.on('line',(lineString) =>{
     const colums = lineString.split(',');
     const year = parseInt(colums[0]);
     const prefecture = colums[2];
     const popu = parseInt(colums[7]);
     if(year === 2010 || year ===　2015){
-        console.log(year);
-        console.log(prefecture);
-        console.log(popu);
+    let  value = map.get(prefecture);
+    if(!value){
+        value = {
+            popu10: 0,
+            popu15: 0,
+            change: null
+        };
+    }
+    if (year === 2010){
+        value.popu10 += popu;
+    }
+    if(year === 2015){
+        value.popu15 += popu;
+    }
+    map.set(prefecture, value);
     }
 });
 rl.resume();
+rl.on('close',() =>{
+    console.log(map);
+});
